b94f0d33788fb635b1d555b319dc12fef96f2de5
Taylor Robins <robinsnom@gmail.com>
2020-06-29 20:13:30 -0600

    Added JS Functionality to list

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 console.log("Hello");
+
+const inputFieldElement = document.getElementById("inputField");
+const listElement = document.getElementById("list");
+const addButtonElement = document.getElementById("addButton");
+
+addButtonElement.addEventListener("click", function () {
+  const todoValue = inputFieldElement.value;
+
+  const newListItem = document.createElement("li");
+  newListItem.innerText = todoValue;
+
+  listElement.appendChild(newListItem);
+
+  inputFieldElement.value = "";
+
+  inputFieldElement.focus();
+
+  console.log(`To Do Item Added to List ${todoValue}`);
+});
+
+inputFieldElement.addEventListener("keypress", function (e) {
+  if (e.key == "Enter") {
+    console.log("enter pressed");
+    const todoValue = inputFieldElement.value;
+
+    const newListItem = document.createElement("li");
+    newListItem.innerText = todoValue;
+
+    listElement.appendChild(newListItem);
+
+    inputFieldElement.value = "";
+
+    inputFieldElement.focus();
+
+    console.log(`To Do Item Added to List ${todoValue}`);
+  }
+});
